d67cd1a276a6fd9cfa75188814f7d4c9510974c9
Akarsh Jaiswal <akarsh.jaiswal@unthinkable.co>
2020-12-10 13:06:07 +0530

    Stable version: tansforming to angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    HeaderComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
